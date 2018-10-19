ab2bd3ea4362b50903a4da30fbfc4242396f105b
xutao <xuyz1988@163.com>
2018-10-19 15:49:14 +0800

    fix 证书多次注册问题
    go.sum从git版本管理中移除
    
    Fixed #24

--- star/app.go
+++ star/app.go
@@ -1,15 +1,20 @@
 package star
 
 import (
+	qosacc "github.com/QOSGroup/qos/account"
 	"github.com/QOSGroup/qstars/baseapp"
 	"github.com/QOSGroup/qstars/wire"
 
-	"github.com/QOSGroup/qstars/x/kvstore"
-	abci "github.com/tendermint/tendermint/abci/types"
-	"github.com/tendermint/tendermint/libs/log"
 	"io"
 	"os"
+
+	"github.com/QOSGroup/qbase/account"
+	"github.com/QOSGroup/qstars/x/kvstore"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/crypto"
+	"github.com/tendermint/tendermint/crypto/ed25519"
 	dbm "github.com/tendermint/tendermint/libs/db"
+	"github.com/tendermint/tendermint/libs/log"
 )
 
 func NewApp(log.Logger, dbm.DB, io.Writer) abci.Application {
@@ -25,6 +30,11 @@ func NewApp(log.Logger, dbm.DB, io.Writer) abci.Application {
 
 func MakeCodec() *wire.Codec {
 	cdc := wire.NewCodec()
+
+	cdc.RegisterInterface((*crypto.PubKey)(nil), nil)
+	cdc.RegisterConcrete(&ed25519.PubKeyEd25519{}, "ed25519.PubKeyEd25519", nil)
+	cdc.RegisterInterface((*account.Account)(nil), nil)
+	cdc.RegisterConcrete(&qosacc.QOSAccount{}, "qbase/account/QOSAccount", nil)
 
 	return cdc
 }
--- x/auth/process.go
+++ x/auth/process.go
@@ -3,13 +3,10 @@
 package auth
 
 import (
-	"github.com/QOSGroup/qbase/account"
 	qosacc "github.com/QOSGroup/qos/account"
 	"github.com/QOSGroup/qstars/client/context"
 	"github.com/QOSGroup/qstars/types"
 	"github.com/QOSGroup/qstars/wire"
-	"github.com/tendermint/tendermint/crypto"
-	"github.com/tendermint/tendermint/crypto/ed25519"
 )
 
 // QueryAccount query account by addr
@@ -20,11 +17,6 @@ func QueryAccount(cdc *wire.Codec, addr string) (*qosacc.QOSAccount, error) {
 	}
 
 	cliCtx := context.NewOQSCLIContext().WithCodec(cdc)
-
-	cdc.RegisterInterface((*crypto.PubKey)(nil), nil)
-	cdc.RegisterConcrete(&ed25519.PubKeyEd25519{}, "ed25519.PubKeyEd25519", nil)
-	cdc.RegisterInterface((*account.Account)(nil), nil)
-	cdc.RegisterConcrete(&qosacc.QOSAccount{}, "qbase/account/QOSAccount", nil)
 
 	acc, err := cliCtx.GetAccount(key)
 	if err != nil {
