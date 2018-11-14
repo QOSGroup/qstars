3aa9927c2c79b8af285362f0c8c6f60c43da6fa1
Matt Wang <wmd_hust@126.com>
2018-11-14 12:23:24 +0800

    On issue #92 change privkey to base64 format in iosapp

--- slim/basic.go
+++ slim/basic.go
@@ -1,8 +1,8 @@
 package slim
 
 import (
-	"encoding/hex"
-	"github.com/QOSGroup/qstars/client/lcd/lib"
+	"encoding/base64"
+	"github.com/QOSGroup/qstars/slim/funcInlocal/respwrap"
 	"github.com/bartekn/go-bip39"
 	"github.com/tendermint/tendermint/crypto/ed25519"
 	"github.com/tendermint/tendermint/libs/bech32"
@@ -25,9 +25,6 @@ const (
 )
 
 func AccountCreate() *ResultCreateAccount {
-	//from the QOS generation methods
-	//	cdc := star.MakeCodec()
-	//	acc := InitKeys(cdc)[2]
 	entropy, _ := bip39.NewEntropy(256)
 	mnemonic, _ := bip39.NewMnemonic(entropy)
 	seedo := bip39.NewSeed(mnemonic, "qstars")
@@ -38,14 +35,16 @@ func AccountCreate() *ResultCreateAccount {
 	addr := key.PubKey().Address()
 	bech32Pub, _ := bech32.ConvertAndEncode(Bech32PrefixAccPub, pub)
 	bech32Addr, _ := bech32.ConvertAndEncode(PREF_ADD, addr.Bytes())
-	privkeyhex := "0x" + hex.EncodeToString(key.Bytes())
+
+	privkeybase64 := base64.StdEncoding.EncodeToString(key.Bytes())
+	//privkeyhex := "0x" + hex.EncodeToString(key.Bytes())
 
 	//Type field for future use
 	Type := AccountResultType
 
 	result := &ResultCreateAccount{}
 	result.PubKey = bech32Pub
-	result.PrivKey = privkeyhex
+	result.PrivKey = privkeybase64
 	result.Addr = bech32Addr
 	result.Mnemonic = mnemonic
 	result.Type = Type
@@ -56,8 +55,57 @@ func AccountCreate() *ResultCreateAccount {
 //convert the output to json string format
 func AccountCreateStr() string {
 	acc := AccountCreate()
-	result, _ := lib.ResponseWrapper(cmCdc, acc, nil)
+	result, _ := respwrap.ResponseWrapper(Cdc, acc, nil)
 	out := string(result)
 	//fmt.Println(out)
+	return out
+}
+
+func AccountRecoverStr(mncode string) string {
+	seed := bip39.NewSeed(mncode, "qstars")
+	key := ed25519.GenPrivKeyFromSecret(seed)
+	pub := key.PubKey().Bytes()
+	addr := key.PubKey().Address()
+	bech32Pub, _ := bech32.ConvertAndEncode("cosmosaccpub", pub)
+	bech32Addr, _ := bech32.ConvertAndEncode(PREF_ADD, addr.Bytes())
+	privkeybase64 := base64.StdEncoding.EncodeToString(key.Bytes())
+	//change privkey output to hex string format
+	//privkeyhex := "0x" + hex.EncodeToString(key.Bytes())
+
+	Type := AccountResultType
+	result := &ResultCreateAccount{}
+	result.PubKey = bech32Pub
+	result.PrivKey = privkeybase64
+	result.Addr = bech32Addr
+	result.Mnemonic = mncode
+	result.Type = Type
+
+	resp, _ := respwrap.ResponseWrapper(Cdc, result, nil)
+	out := string(resp)
+	return out
+}
+
+type PubAddrRetrieval struct {
+	PubKey string `json:"pubKey"`
+	Addr   string `json:"addr"`
+}
+
+func PubAddrRetrievalStr(s string) string {
+	//the privkey output was in hex string format, decode it with the same decoding
+	//bz, _ := hex.DecodeString(s[2:])
+	bz, _ := base64.StdEncoding.DecodeString(s)
+	var key ed25519.PrivKeyEd25519
+	Cdc.MustUnmarshalBinaryBare(bz, &key)
+	pub := key.PubKey().Bytes()
+	addr := key.PubKey().Address()
+	bech32Pub, _ := bech32.ConvertAndEncode(Bech32PrefixAccPub, pub)
+	bech32Addr, _ := bech32.ConvertAndEncode(PREF_ADD, addr.Bytes())
+
+	result := &PubAddrRetrieval{}
+	result.PubKey = bech32Pub
+	result.Addr = bech32Addr
+
+	resp, _ := respwrap.ResponseWrapper(Cdc, result, nil)
+	out := string(resp)
 	return out
 }
