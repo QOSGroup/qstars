d4ded11a6c5e2b8bc9316023ad44ebcc653227c3
Matt Wang <wmd_hust@126.com>
2018-11-19 17:33:09 +0800

    Issue #75 to make the project slim, to be continued... slim local part refactored ready, send to Hao for size evaluation

--- slim/iosapp/iosapp.go
+++ slim/iosapp/iosapp.go
@@ -1,7 +1,6 @@
 package iosapp
 
 import (
-	"fmt"
 	"github.com/QOSGroup/qstars/slim"
 )
 
@@ -11,7 +10,20 @@ func AccountCreate() string {
 	return output
 }
 
-func QSCtransfer() {
-	output := slim.QSCtransferSendStr("address12as5uhdpf2y9zjkurx2l6dz8g98qkgryc4x355", "22qos", "0xa328891040ae9b773bcd30005235f99a8d62df03a89e4f690f9fa03abb1bf22715fc9ca05613f2d8061492e9f8149510b5b67d340d199ff24f34c85dbbbd7e0df780e9a6cc", "qos-testapp")
-	fmt.Println(output)
+//for AccountRecovery
+func AccountRecover(mncode string) string {
+	output := slim.AccountRecoverStr(mncode)
+	return output
 }
+
+//for PubAddrRetrieval
+func PubAddrRetrieval(priv string) string {
+	//	fmt.Println("Please input host including IP and port for initialization on Qstar deamon:")
+	output := slim.PubAddrRetrievalStr(priv)
+	return output
+}
+
+//func QSCtransfer() {
+//	output := slim.QSCtransferSendStr("address12as5uhdpf2y9zjkurx2l6dz8g98qkgryc4x355", "22qos", "0xa328891040ae9b773bcd30005235f99a8d62df03a89e4f690f9fa03abb1bf22715fc9ca05613f2d8061492e9f8149510b5b67d340d199ff24f34c85dbbbd7e0df780e9a6cc", "qos-testapp")
+//	fmt.Println(output)
+//}
--- slim/wire.go
+++ slim/wire.go
@@ -1,113 +1,145 @@
 package slim
 
 import (
-	"github.com/QOSGroup/qbase/account"
-	"github.com/QOSGroup/qbase/baseabci"
-	"github.com/QOSGroup/qbase/context"
-	"github.com/QOSGroup/qbase/mapper"
-	"github.com/QOSGroup/qbase/txs"
+	"github.com/QOSGroup/qstars/slim/funcInlocal/ed25519local"
 	"github.com/tendermint/go-amino"
-	"github.com/tendermint/tendermint/crypto"
-	"github.com/tendermint/tendermint/crypto/ed25519"
-	cmn "github.com/tendermint/tendermint/libs/common"
-	"log"
-	"reflect"
 )
 
-// amino codec to marshal/unmarshal
-var typeRegistry = make(map[string]reflect.Type)
-var Cdc *amino.Codec
+//
+//import (
+//	"github.com/QOSGroup/qbase/account"
+//	"github.com/QOSGroup/qbase/baseabci"
+//	"github.com/QOSGroup/qbase/context"
+//	"github.com/QOSGroup/qbase/mapper"
+//	"github.com/QOSGroup/qbase/txs"
+//	"github.com/tendermint/go-amino"
+//	"github.com/tendermint/tendermint/crypto"
+//	"github.com/tendermint/tendermint/crypto/ed25519"
+//	cmn "github.com/tendermint/tendermint/libs/common"
+//	"log"
+//	"reflect"
+//)
+//
+type Codec = amino.Codec
 
-type ABCICodeType uint32
-type Tags cmn.KVPairs
-
-type QstarsBaseApp struct {
-	Transactions    BaseXTransaction
-	Baseapp         *baseabci.BaseApp
-	TransactionList []BaseXTransaction
-	Logger          log.Logger
-	RootDir         string
-}
-type BaseXTransaction interface {
-	mapper.IMapper
-	RegisterCdc(cdc *amino.Codec)
-	StartX(base *QstarsBaseApp) error
-	ResultNotify(ctx context.Context, txQcpResult interface{}) *Result
-}
-type Result struct {
-
-	// Code is the response code, is stored back on the chain.
-	Code ABCICodeType
-
-	// Data is any data returned from the app.
-	Data []byte
-
-	// Log is just debug information. NOTE: nondeterministic.
-	Log string
-
-	// GasWanted is the maximum units of work we allow this tx to perform.
-	GasWanted int64
-
-	// GasUsed is the amount of gas actually consumed. NOTE: unimplemented
-	GasUsed int64
-
-	// Tx fee amount and denom.
-	FeeAmount int64
-	FeeDenom  string
-
-	// Tags are used for transaction indexing and pubsub.
-	Tags Tags
-}
-
-func MakeCodec() *amino.Codec {
-	cdc := MakeQBaseCodec()
-	for k, _ := range typeRegistry {
-		txs, err := newStruct(k)
-		if err == false {
-			panic("reflect transaction is error.")
-		}
-		t := txs.(BaseXTransaction)
-		t.RegisterCdc(cdc)
-	}
-	//kvstore.NewKVStub().RegisterKVCdc(cdc)
-	//bank.NewBankStub().RegisterKVCdc(cdc)
+func NewCodec() *Codec {
+	cdc := amino.NewCodec()
 	return cdc
 }
 
-func newStruct(name string) (interface{}, bool) {
-	elem, ok := typeRegistry[name]
-	if !ok {
-		return nil, false
-	}
-	return reflect.New(elem).Elem().Interface(), true
-}
+var Cdc *Codec
 
 func init() {
-	Cdc = MakeCodec()
-}
-
-func MakeQBaseCodec() *amino.Codec {
-
-	var cdc = amino.NewCodec()
+	cdc := NewCodec()
 	RegisterAmino(cdc)
-	RegisterCodec(cdc)
-
-	return cdc
+	Cdc = cdc.Seal()
 }
 
-func RegisterCodec(cdc *amino.Codec) {
-	txs.RegisterCodec(cdc)
-	account.RegisterCodec(cdc)
-}
-
+// RegisterAmino registers all crypto related types in the given (amino) codec.
 func RegisterAmino(cdc *amino.Codec) {
-	// These are all written here instead of
-	cdc.RegisterInterface((*crypto.PubKey)(nil), nil)
-	cdc.RegisterConcrete(ed25519.PubKeyEd25519{},
-		"tendermint/PubKeyEd25519", nil)
+	cdc.RegisterInterface((*ed25519local.PubKey)(nil), nil)
+	cdc.RegisterConcrete(ed25519local.PubKeyEd25519{},
+		ed25519local.Ed25519PubKeyAminoRoute, nil)
 
-	cdc.RegisterInterface((*crypto.PrivKey)(nil), nil)
-	cdc.RegisterConcrete(ed25519.PrivKeyEd25519{},
-		"tendermint/PrivKeyEd25519", nil)
-
+	cdc.RegisterInterface((*ed25519local.PrivKey)(nil), nil)
+	cdc.RegisterConcrete(ed25519local.PrivKeyEd25519{},
+		ed25519local.Ed25519PrivKeyAminoRoute, nil)
 }
+
+// amino codec to marshal/unmarshal
+//var typeRegistry = make(map[string]reflect.Type)
+//var Cdc *amino.Codec
+//
+//type ABCICodeType uint32
+//type Tags cmn.KVPairs
+//
+//type QstarsBaseApp struct {
+//	Transactions    BaseXTransaction
+//	Baseapp         *baseabci.BaseApp
+//	TransactionList []BaseXTransaction
+//	Logger          log.Logger
+//	RootDir         string
+//}
+//type BaseXTransaction interface {
+//	mapper.IMapper
+//	RegisterCdc(cdc *amino.Codec)
+//	StartX(base *QstarsBaseApp) error
+//}
+//type Result struct {
+//
+//	// Code is the response code, is stored back on the chain.
+//	Code ABCICodeType
+//
+//	// Data is any data returned from the app.
+//	Data []byte
+//
+//	// Log is just debug information. NOTE: nondeterministic.
+//	Log string
+//
+//	// GasWanted is the maximum units of work we allow this tx to perform.
+//	GasWanted int64
+//
+//	// GasUsed is the amount of gas actually consumed. NOTE: unimplemented
+//	GasUsed int64
+//
+//	// Tx fee amount and denom.
+//	FeeAmount int64
+//	FeeDenom  string
+//
+//	// Tags are used for transaction indexing and pubsub.
+//	Tags Tags
+//}
+//
+//func MakeCodec() *amino.Codec {
+//	cdc := MakeQBaseCodec()
+//	for k, _ := range typeRegistry {
+//		txs, err := newStruct(k)
+//		if err == false {
+//			panic("reflect transaction is error.")
+//		}
+//		t := txs.(BaseXTransaction)
+//		t.RegisterCdc(cdc)
+//	}
+//	//kvstore.NewKVStub().RegisterKVCdc(cdc)
+//	//bank.NewBankStub().RegisterKVCdc(cdc)
+//	return cdc
+//}
+//
+////
+//func newStruct(name string) (interface{}, bool) {
+//	elem, ok := typeRegistry[name]
+//	if !ok {
+//		return nil, false
+//	}
+//	return reflect.New(elem).Elem().Interface(), true
+//}
+//
+//func init() {
+//	Cdc = MakeCodec()
+//}
+//
+//func MakeQBaseCodec() *amino.Codec {
+//
+//	var cdc = amino.NewCodec()
+//	//RegisterAmino(cdc)
+//	RegisterCodec(cdc)
+//
+//	return cdc
+//}
+//
+//func RegisterCodec(cdc *amino.Codec) {
+//	//txs.RegisterCodec(cdc)
+//	//account.RegisterCodec(cdc)
+//}
+
+//func RegisterAmino(cdc *amino.Codec) {
+//	// These are all written here instead of
+//	cdc.RegisterInterface((*crypto.PubKey)(nil), nil)
+//	cdc.RegisterConcrete(ed25519.PubKeyEd25519{},
+//		"tendermint/PubKeyEd25519", nil)
+//
+//	cdc.RegisterInterface((*crypto.PrivKey)(nil), nil)
+//	cdc.RegisterConcrete(ed25519.PrivKeyEd25519{},
+//		"tendermint/PrivKeyEd25519", nil)
+//
+//}
