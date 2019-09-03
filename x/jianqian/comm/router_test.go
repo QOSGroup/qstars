65a4a5072b1b7c98c8c47d490435733d20846dbc
mazhong <ms892@163.com>
2019-09-03 14:12:24 +0800

    fix

--- x/jianqian/comm/router_test.go
+++ x/jianqian/comm/router_test.go
@@ -7,7 +7,7 @@ func TestGetStruct(t *testing.T) {
 	args := []string{"address103cfpkrsw8yu78gem6mg26rkyqsfmnr5sj0hvp", "ATOM", "2", "10000"}
 	//args:=[]string{"address103cfpkrsw8yu78gem6mg26rkyqsfmnr5sj0hvp","1000","aaaaa","atom"}
 
-	tx, err := getStruct(ExtractTxFlag, args)
+	tx, err := getStruct(ExtractTxFlag, args,nil)
 
 	t.Log(err)
 
